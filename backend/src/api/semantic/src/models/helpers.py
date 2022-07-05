f68815122b44b07afc83511830fa23f5cad7ba78
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-07-05 19:39:40 +0200

    Update helpers.py

--- backend/src/api/semantic/src/models/helpers.py
+++ backend/src/api/semantic/src/models/helpers.py
@@ -1,24 +1,24 @@
-from pyarabic.araby import tokenize
+# from pyarabic.araby import tokenize
 
-stopwords = []
-def get_stopwords():
-  # prepare/read stopwords
-  stopwords = open("./data/external/stopwords_list.txt").readlines()
+# stopwords = []
+# def get_stopwords():
+#   # prepare/read stopwords
+#   stopwords = open("./data/external/stopwords_list.txt").readlines()
 
-  # remove /n from each stopword
-  stopwords = [word[:-1] for word in stopwords]
+#   # remove /n from each stopword
+#   stopwords = [word[:-1] for word in stopwords]
   
-  # convert list to set, to enhance searching
-  stopwords = set(stopwords)
-  return stopwords
+#   # convert list to set, to enhance searching
+#   stopwords = set(stopwords)
+#   return stopwords
 
 
-def get_meaningful_words_length(text):
-  tokens = tokenize(text)
+# def get_meaningful_words_length(text):
+#   tokens = tokenize(text)
 
-  cnt = 0
-  for term in tokens:
-    if term not in stopwords:
-      cnt += 1
+#   cnt = 0
+#   for term in tokens:
+#     if term not in stopwords:
+#       cnt += 1
       
-  return cnt
+#   return cnt
